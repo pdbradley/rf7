7e77688f00b7a9b8a04c7039521ab43a5fec9b8b
Philip Bradley <pdbradley@gmail.com>
2016-10-18 15:58:50 -0400

    eric page

---
+++ app/assets/javascripts/phone/setup.js
@@ -0,0 +1,22 @@
+var myApp = new Framework7();
+
+var $$ = Dom7;
+
+var mainView = myApp.addView('.view-main', {
+  dynamicNavbar: true,
+  cache: false
+});
+
+
+// window.F7H =
+//   app: new Framework7()
+//   # dom: Framework7.$
+
+// window.Phone =
+//   Views: {}
+
+// Phone.Views.main =
+//   F7H.app.addView '.view-main',
+//     dynamicNavbar: true
+
+
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def eric
+    render layout: false
+  end
+
+  def show
+    render layout: false
+  end
+
+  def about
+    render layout: false
+  end
+end
