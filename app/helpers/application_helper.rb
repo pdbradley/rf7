d603fff48ea2cc9948395522ef470f91ea7dcaf6
Philip Bradley <pdbradley@gmail.com>
2016-10-18 17:39:23 -0400

    playing with the navbar helper wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+
+  def f7_navbar(left: nil, center: nil, right: nil, sliding: true)
+    content_tag :div, class: :navbar  do
+      content_tag :div, class: 'navbar-inner' do
+        concat content_tag :div, left, class: "left" if left
+        concat content_tag :div, center, class: "center" if center
+        concat content_tag :div, right, class: "right" if right
+      end
+    end
+  end
 end
+
+
